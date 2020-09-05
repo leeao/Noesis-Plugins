733e7a40f560c845afd979651a6b26fc451635d9
leeao <leeao@live.com>
2020-09-05 09:55:09 +0800

    added read chunkType 0xb

--- Textures/tex_TwistedMetalBlack(PS2)_tex.py
+++ Textures/tex_TwistedMetalBlack(PS2)_tex.py
@@ -23,12 +23,14 @@ def noepyLoadRGBA(data, texList):
         numTextures += typeNumTex
     #print(numTextures)
     bs.seek(texOfs)
+    i = 0
     #for i in range(numTextures):
     while(bs.tell()<bs.getSize()):
         tex = Texture(bs)
         tex.read()
+        nextOfs = bs.tell()
         if tex.chunkFlag == 8:
-            if tex.isPALImage:
+            if(bs.tell()<bs.getSize()):
                 texPal = Texture(bs)
                 texPal.read()
                 if texPal.chunkFlag == 0x88:
@@ -39,7 +41,8 @@ def noepyLoadRGBA(data, texList):
                             palette += rgba32(texPal.palette[p])                           
                         pixel = rapi.imageUntwiddlePS2(tex.pixel, tex.width, tex.height, 8)
                         textureData = rapi.imageDecodeRawPal(pixel,palette,tex.width,tex.height,8,"r8g8b8a8", noesis.DECODEFLAG_PS2SHIFT)
-                        texList.append(NoeTexture(rapi.getInputName()+str(i), tex.width, tex.height, textureData, noesis.NOESISTEX_RGBA32))
+                        texName = "%d_8bpp_x%x_x%x"%(i,tex.texType,tex.chunkFlag)
+                        texList.append(NoeTexture(texName, tex.width, tex.height, textureData, noesis.NOESISTEX_RGBA32))
                     elif texPal.colors == 16:
                         #print("PAL4") 
                         palette = bytes()
@@ -47,19 +50,42 @@ def noepyLoadRGBA(data, texList):
                             palette += rgba32(texPal.palette[p])                
                         
                         textureData = rapi.imageDecodeRawPal(tex.pixel,palette,tex.width,tex.height,4,"r8g8b8a8")
-                        texList.append(NoeTexture(rapi.getInputName()+str(i), tex.width, tex.height, textureData, noesis.NOESISTEX_RGBA32))
-            elif tex.bpp == 32 :
+                        texName = "%d_4bpp_x%x_x%x"%(i,tex.texType,tex.chunkFlag)
+                        texList.append(NoeTexture(texName, tex.width, tex.height, textureData, noesis.NOESISTEX_RGBA32))
+                else:
+                    bs.seek(nextOfs) #back to next
+                    tex.bpp = 32
+                    width = tex.width // 2
+                    height = tex.height // 2
+                    pixel = parseRgba32(tex.pixel,width,height)
+                    textureData = rapi.imageDecodeRaw(pixel,width,height,"r8g8b8a8")
+                    texName = "%d_32bpp_x%x_x%x"%(i,tex.texType,tex.chunkFlag)
+                    texList.append(NoeTexture(texName, width,height, textureData, noesis.NOESISTEX_RGBA32))                    
+            else:
+                tex.bpp = 32
                 width = tex.width // 2
                 height = tex.height // 2
                 pixel = parseRgba32(tex.pixel,width,height)
                 textureData = rapi.imageDecodeRaw(pixel,width,height,"r8g8b8a8")
-                texList.append(NoeTexture(rapi.getInputName()+str(i), width,height, textureData, noesis.NOESISTEX_RGBA32))
+                texName = "%d_32bpp_x%x_x%x"%(i,tex.texType,tex.chunkFlag)
+                texList.append(NoeTexture(texName, width,height, textureData, noesis.NOESISTEX_RGBA32))
+        elif tex.chunkFlag & 0xB == 0xB:
+            tex.bpp = 32
+            width = tex.width // 2
+            height = tex.height // 2
+            pixel = parseRgba32(tex.pixel,width,height)
+            textureData = rapi.imageDecodeRaw(pixel,width,height,"r8g8b8a8")
+            texName = "%d_32bpp_x%x_x%x"%(i,tex.texType,tex.chunkFlag)
+            texList.append(NoeTexture(texName, width,height, textureData, noesis.NOESISTEX_RGBA32))            
         elif tex.chunkFlag & 0x20 == 0x20:
-                textureData = rapi.imageDecodeRaw(tex.pixel,tex.width,tex.height,"a8")
-                texList.append(NoeTexture(rapi.getInputName()+str(i), tex.width,tex.height, textureData, noesis.NOESISTEX_RGBA32))
+            textureData = rapi.imageDecodeRaw(tex.pixel,tex.width,tex.height,"a8")
+            texName = "%d_A8_x%x_x%x"%(i,tex.texType,tex.chunkFlag)
+            texList.append(NoeTexture(texName, tex.width,tex.height, textureData, noesis.NOESISTEX_RGBA32))
         elif tex.chunkFlag & 0x40 == 0x40:
-                textureData = rapi.imageDecodeRaw(tex.pixel,tex.width,tex.height,"a4")
-                texList.append(NoeTexture(rapi.getInputName()+str(i), tex.width,tex.height, textureData, noesis.NOESISTEX_RGBA32))
+            textureData = rapi.imageDecodeRaw(tex.pixel,tex.width,tex.height,"a4")
+            texName = "%d_A4_x%x_x%x"%(i,tex.texType,tex.chunkFlag)
+            texList.append(NoeTexture(texName, tex.width,tex.height, textureData, noesis.NOESISTEX_RGBA32))
+        i += 1    
     return 1
 
 
@@ -72,6 +98,7 @@ class Texture(object):
         self.height= 0
         self.colors = 0
         self.chunkFlag = 0
+        self.texType = 0
         self.pixel = bytes()
         self.palette = bytes()
         self.isPALImage = True
@@ -83,17 +110,18 @@ class Texture(object):
         unk2 = self.bs.readShort()
         unk3 = self.bs.readShort()
         unk4 = self.bs.readInt()
-        palFlag = self.bs.readByte()
+        texType = self.bs.readByte() 
         sizeFlag = self.bs.readByte()
         chunkFlag = self.bs.readUByte()
         unkFlag = self.bs.readByte()
         self.chunkFlag = chunkFlag
+        self.texType = texType
         self.bs.seek(112,1)
 
         self.height =  2 << ((sizeFlag & 0xf0) >> 4)
         self.width = 2 << (sizeFlag & 0xf)
         if chunkFlag == 0x8:      
-            if self.width > 256:
+            if texType & 0x40 == 0x40:
                 self.isPALImage = False
                 self.bpp = 32
         if chunkFlag & 0x20 == 0x20 or chunkFlag & 0x40 == 0x40 :
